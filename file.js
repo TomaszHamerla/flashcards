689ae897922ab6ba3058a4a278f7358f5f593053
Tomasz Hamerla <tomaszhamerla@interia.pl>
2023-12-27 16:52:22 +0100

    refactored code

--- file.js
+++ file.js
@@ -1,7 +1,3 @@
-const cards = document.querySelectorAll(".flashcard");
-cards.forEach((card) => card.addEventListener("mouseenter", getCardsEn));
-cards.forEach((card) => card.addEventListener("mouseout", getCardsPl));
-
 const flashcardsData = [
   { polish: "kot", english: "cat" },
   { polish: "pies", english: "dog" },
@@ -9,24 +5,25 @@ const flashcardsData = [
   { polish: "telefon", english: "phone" },
   { polish: "muzyka", english: "music" },
 ];
-cards.forEach((card, index) => {
-  if (flashcardsData[index]) {
-    const { polish } = flashcardsData[index];
-    card.innerHTML = polish;
-  } else {
-    card.innerHTML = "No data available";
-  }
+const container = document.getElementById("container");
+
+flashcardsData.forEach((flashcard) => {
+  createFlashcards(flashcard.polish, flashcard.english);
 });
-function getCardsPl(event) {
-  const cardIndex = Array.from(cards).indexOf(event.currentTarget);
-  const { polish } = flashcardsData[cardIndex];
-  cards[cardIndex].innerHTML = polish;
-  cards[cardIndex].setAttribute("style", "flashcard");
-}
-function getCardsEn(event) {
-  const cardIndex = Array.from(cards).indexOf(event.currentTarget);
-  const { english } = flashcardsData[cardIndex];
-  console.log(english);
-  cards[cardIndex].innerHTML = english;
-  cards[cardIndex].setAttribute("style", "color: blue;font-size: 50px");
+
+function createFlashcards(polish, english) {
+  const flashcard = document.createElement("div");
+  flashcard.classList.add("flashcard");
+  container.appendChild(flashcard);
+  flashcard.textContent = polish;
+
+  flashcard.addEventListener("mouseenter", () => {
+    flashcard.textContent = english;
+    flashcard.setAttribute("style", "color:blue");
+  });
+
+  flashcard.addEventListener("mouseout", () => {
+    flashcard.textContent = polish;
+    flashcard.setAttribute("style", "flashcard");
+  });
 }
